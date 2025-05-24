d79916de75120df8a1dbfb335168a98636bfb150
kishu279 <souravpoddar6677@gmail.com>
2025-05-25 01:13:46 +0530

    local data persist

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+import { prismaClient } from "@/lib/db";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -10,7 +11,45 @@ const handler = NextAuth({
   ],
 
   secret: process.env.NEXTAUTH_SECRET ?? "secret",
+
+  callbacks: {
+    async signIn({
+      account,
+      profile,
+      user,
+    }: {
+      user: any;
+      account: any | null;
+      profile?: any;
+      email?: { verificationRequest?: boolean };
+      credentials?: Record<string, unknown>;
+    }): Promise<boolean> {
+      if (account.provider === "google") {
+        if (profile.email_verified) {
+          const existingUser = await prismaClient.user.findUnique({
+            where: { email: profile.email },
+          });
+
+          if (!existingUser) {
+            await prismaClient.user.create({
+              data: {
+                email: user.email,
+                name: user.name ?? "user",
+                image: user.image ?? "",
+                provider: "Google",
+              },
+            });
+          }
+
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      return false;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
-
